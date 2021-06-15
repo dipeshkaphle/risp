23c59d638d60a6ce75bdc88570d4e078e6d0fd68
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2021-06-15 13:57:39 +0545

    Basic if and define expressions added

--- src/eval.rs
+++ src/eval.rs
@@ -1,5 +1,12 @@
 use super::parser::*;
 use super::types::*;
+
+pub fn parse_and_eval(program: String, env: &mut Env) -> Result<Exp, Exceptions> {
+    let parsed_exp = parse(program)?;
+    let eval_exp = eval(&parsed_exp, env)?;
+    Ok(eval_exp)
+}
+
 pub fn eval(exp: &Exp, env: &mut Env) -> Result<Exp, Exceptions> {
     match &exp {
         Exp::Atom(x) => match x {
@@ -16,15 +23,33 @@ pub fn eval(exp: &Exp, env: &mut Env) -> Result<Exp, Exceptions> {
                 ));
             } else {
                 let (first, rest) = x.split_first().unwrap();
-                let f = eval(first, env)?;
-                if let Exp::Func(function) = f {
-                    let rest_evaluated: Result<Vec<Exp>, Exceptions> =
-                        rest.iter().map(|x| eval(x, env)).collect();
-                    return function(&rest_evaluated?);
-                } else {
-                    return Err(Exceptions::ValueError(
-                        "First arg should be a function".to_string(),
-                    ));
+
+                match first {
+                    Exp::Atom(Atom::Symbol(s)) => match &s[..] {
+                        "if" => if_handler(x, env),
+                        "define" => define_handler(x, env),
+                        _ => {
+                            let f = eval(first, env)?;
+                            if let Exp::Func(function) = f {
+                                let rest_evaluated: Result<Vec<Exp>, Exceptions> =
+                                    rest.iter().map(|x| eval(x, env)).collect();
+                                return function(&rest_evaluated?);
+                            } else {
+                                return Err(Exceptions::ValueError(
+                                    format!("{} is not a defined as a function", s).to_string(),
+                                ));
+                            }
+                        }
+                    },
+                    _ => {
+                        return Err(Exceptions::ValueError(
+                            format!(
+                                "First thing in a list should be a keyword or a function not {}",
+                                first
+                            )
+                            .to_string(),
+                        ));
+                    }
                 }
             }
         }
@@ -32,8 +57,46 @@ pub fn eval(exp: &Exp, env: &mut Env) -> Result<Exp, Exceptions> {
     }
 }
 
-pub fn parse_and_eval(program: String, env: &mut Env) -> Result<Exp, Exceptions> {
-    let parsed_exp = parse(program)?;
-    let eval_exp = eval(&parsed_exp, env)?;
-    Ok(eval_exp)
+fn if_handler(args: &[Exp], env: &mut Env) -> Result<Exp, Exceptions> {
+    let x = args;
+    if x.len() == 4 {
+        let (test, conseq, alt) = (&x[1], &x[2], &x[3]);
+        if let Exp::Atom(Atom::Bool(test_evaluated)) = eval(test, env)? {
+            if test_evaluated {
+                return eval(conseq, env);
+            } else {
+                return eval(alt, env);
+            }
+        } else {
+            return Err(Exceptions::ValueError(
+                format!("{} doesnt evaluate to a boolean", test).to_string(),
+            ));
+        }
+    } else {
+        return Err(Exceptions::ValueError(
+            "Not a valid if expression".to_string(),
+        ));
+    }
+}
+
+fn define_handler(args: &[Exp], env: &mut Env) -> Result<Exp, Exceptions> {
+    if args.len() == 3 {
+        let (symbol, exp) = (&args[1], &args[2]);
+        if let Exp::Atom(Atom::Symbol(x)) = symbol {
+            env.insert(x.clone(), exp.clone());
+            Ok(exp.clone())
+        } else {
+            return Err(Exceptions::ValueError(
+                format!(
+                    "define expression should have its first argument as a symbol not a {}",
+                    symbol
+                )
+                .to_string(),
+            ));
+        }
+    } else {
+        return Err(Exceptions::ValueError(
+            "Not a valid define expression".to_string(),
+        ));
+    }
 }
